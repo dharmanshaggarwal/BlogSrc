19875bcd9f4f11eab1bb79e12e53c6a445f5f716
sushobitha <sushobhithaps@gmail.com>
2019-06-05 20:58:30 +0530

    dist

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { BlogComponent } from "./blog/blog.component";
+import { HomeComponent } from "./home/home.component";
+
+const routes: Routes = [
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "home", component: HomeComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
